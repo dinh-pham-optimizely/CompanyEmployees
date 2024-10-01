6cb9f418b1be3d3683c8c81f70283a346518255e
DinhPham <dinh.pham@optimizely.com>
2024-10-01 10:19:29 +0700

    Add sorting for employees and companies

---
+++ Repository/Extensions/Utility/OrderQueryBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Repository.Extensions.Utility;
+
+public static class OrderQueryBuilder
+{
+    public static string CreateOrderQuery<T>(string orderByQueryString)
+    {
+        // For example: orderByQueryString is name, age desc.
+
+        // Split query string by comma.
+        // ["name", "age desc"]
+        var orderParams = orderByQueryString.Trim().Split(',');
+        // Get Employee properties info using relfection.
+        var propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var orderQueryBuilder = new StringBuilder();
+
+        foreach (var param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+            {
+                continue;
+            }
+
+            // get field in query.
+            // name and age.
+            var propertyFromQueryName = param.Split(" ")[0];
+
+            // extract property.
+            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (objectProperty is null)
+            {
+                continue;
+            }
+
+            // get the direction.
+            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+        }
+
+        // "Name ascending, Age descending"
+        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+        return orderQuery;
+    }
+}
