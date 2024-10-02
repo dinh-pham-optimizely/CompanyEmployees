72e8a1663f1ddb4533730c0dc92494cf3e9c9624
DinhPham <dinh.pham@optimizely.com>
2024-10-02 13:36:02 +0700

    Implement swagger

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -16,6 +16,7 @@ namespace CompanyEmployees.Presentation.Controllers;
 [ApiVersion("1.0")]
 [Route("api/companies")]
 [ApiController]
+[ApiExplorerSettings(GroupName = "v1")]
 public class CompaniesController : ControllerBase
 {
     private readonly IServiceManager _service;
@@ -23,11 +24,12 @@ public class CompaniesController : ControllerBase
     public CompaniesController(IServiceManager service) => _service = service;
 
     // Last step: Define an api endpoint to get all companies
-    [HttpGet]
+    [HttpGet(Name = "GetCompanies")]
     /*[ResponseCache(Duration = 60)]*/
     [DisableRateLimiting]
     [OutputCache(Duration = 60)]
     [Authorize(Roles = "Manager")]
+    [ProducesResponseType<IEnumerable<CompanyDto>>(200)]
     public async Task<IActionResult> GetCompanies([FromQuery] CompanyParameters companyParameters)
     {
         var pagedResult = await _service.CompanyService.GetAllCompaniesAsync(companyParameters, trackChanges: false);
@@ -40,14 +42,18 @@ public class CompaniesController : ControllerBase
     /*[ResponseCache(CacheProfileName = "120SecondsDuration")]*/
     [OutputCache(PolicyName = "120SecondsDuration")]
     [EnableRateLimiting(policyName: "SpecificPolicy")]
+    [ProducesResponseType<CompanyDto>(200)]
     public async Task<IActionResult> GetCompany(Guid id)
     {
         var company = await _service.CompanyService.GetCompanyAsync(id, trackChanges: false);
         return Ok(company);
     }
 
-    [HttpPost]
+    [HttpPost(Name = "CreateCompany")]
     [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [ProducesResponseType<CompanyDto>(201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(422)]
     public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
     {
         var createdCompany = await _service.CompanyService.CreateCompanyAsync(company);
@@ -57,6 +63,7 @@ public class CompaniesController : ControllerBase
     }
 
     [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+    [ProducesResponseType<IEnumerable<CompanyDto>>(200)]
     public async Task<IActionResult> GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
     {
         var companies = await _service.CompanyService.GetByIdsAsync(ids, trackChanges: false);
@@ -64,7 +71,10 @@ public class CompaniesController : ControllerBase
         return Ok(companies);
     }
 
-    [HttpPost("collection")]
+    [HttpPost("collection", Name = "CreateCompanyCollection")]
+    [ProducesResponseType<IEnumerable<CompanyDto>>(201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(422)]
     public async Task<IActionResult> CreateCompanyColletion([FromBody] IEnumerable<CompanyForCreationDto> companies)
     {
         var result = await _service.CompanyService.CreateCompanyCollectionAsync(companies, false);
@@ -72,7 +82,8 @@ public class CompaniesController : ControllerBase
         return CreatedAtRoute("CompanyCollection", new { result.ids }, result.companies);
     }
 
-    [HttpDelete("{id:guid}")]
+    [HttpDelete("{id:guid}", Name = "DeleteCompany")]
+    [ProducesResponseType(204)]
     public async Task<IActionResult> DeleteCompany(Guid id)
     {
         await _service.CompanyService.DeleteCompanyAsync(id, false);
@@ -80,8 +91,9 @@ public class CompaniesController : ControllerBase
         return NoContent();
     }
 
-    [HttpPut("{id:guid}")]
+    [HttpPut("{id:guid}", Name = "UpdateCompany")]
     [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [ProducesResponseType(204)]
     public async Task<IActionResult> UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
     {
         await _service.CompanyService.UpdateCompanyAsync(id, company, trackChanges: true);
---
+++ Shared/DataTransferObjects/MedataDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Shared.DataTransferObjects;
+
+public record MedataDataDto(int CurrentPage, int TotalPages, int PageSize, int TotalCount, bool HasPrevious, bool HasNext);
